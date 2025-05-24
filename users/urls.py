bb8adb2b8374691bd3ce7545e0ba259416b1f30c
kussal404 <donkushal59@gmail.com>
2025-05-24 10:17:20 +0545

    Delete users directory

--- users/urls.py
+++
@@ -1,9 +0,0 @@
-from .views import RegisterView,LoginView,khalti
-from django.urls import path
-
-urlpatterns = [
-     path('register/',RegisterView.as_view(),name="Register"),
-     path('login/',LoginView.as_view(),name="Login"),
-     path('khalti/',khalti,name="Payment"),
- ]
- 
