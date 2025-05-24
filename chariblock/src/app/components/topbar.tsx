6f631acac9b94401d5e4795d55cbf6bf3018ad81
Kushal-acharya <https://github.com/kushal2060>
2025-05-24 09:51:07 +0545

    Initial

---
+++ chariblock/src/app/components/topbar.tsx
@@ -0,0 +1,12 @@
+
+export default function TopBar() {
+  return (
+    <div className="bg-[#D99CA2] flex justify-end px-4 py-1 mt-7 rounded-lg">
+      <button className="bg-yellow-300 text-sm px-4 py-1 rounded flex items-center space-x-1">
+        
+        <span>Login</span>
+      </button>
+    </div>
+   
+  );
+}
